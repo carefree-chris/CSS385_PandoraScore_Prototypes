ef61edf0a0ec7c9f71d949bf8c15e23be074fdb9
Trayce <TrayceLuxtrum@gmail.com>
2017-05-02 20:10:21 -0700

    Updated Room Prototype Created Sound Prototype

---
+++ Assets/Scripts/EnemyAudio.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAudio : MonoBehaviour {
+
+    private AudioSource a;
+    private float T = 2;
+
+	// Use this for initialization
+	void Start () {
+        a = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            if (T < 0)
+            {
+                a.Play();
+                T = 2;
+            }
+            Debug.Log(T);
+            T -= Time.deltaTime;
+        }
+	}
+}
